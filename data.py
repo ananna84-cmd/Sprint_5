d7cd11aa846962a5bf81ec4a7c16b6be33939dc3
Anna An <anna-26@yandex.ru>
2026-06-14 18:36:29 +0300

    Fix test files and add data.py

---
+++ data.py
@@ -0,0 +1,19 @@
+# Главная страница
+def main_page():
+    return "https://stellarburgers.education-services.ru/"
+
+# Страница регистрации
+def registration_page():
+    return "https://stellarburgers.education-services.ru/register"
+
+# Страница входа
+def login_page():
+    return "https://stellarburgers.education-services.ru/login"
+
+# Страница профиля
+def cabinet_page():
+    return "https://stellarburgers.education-services.ru/account/profile"
+
+# Страница восстановления пароля
+def forgot_password_page():
+    return "https://stellarburgers.education-services.ru/forgot-password"
